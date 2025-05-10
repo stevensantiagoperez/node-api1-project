b10b51943f35569cadeadda0589761f8203aad08
Steven Santiago-Perez <santiagoperezsteven@gmail.com>
2025-05-10 15:31:18 -0400

    post working

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const User = require('./users/model')
 const server = express()
+server.use(express.json())
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -36,7 +37,28 @@ server.get('/api/users/:id', (req, res) => {
         })
 })
 
+server.post('/api/users', (req, res) =>{
+    const user = req.body;
+    if(!user.name || !user.bio){
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else{
+        User.insert(user)
+        .then(createdUser =>{
+            res.status(201).json(createdUser)
+        })
+        .catch(err=> {
+            res.status(500).json({
+                message: 'error getting user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+    
 
+})
 
 server.use('*' , (req, res) => {
     res.status(404).json({
